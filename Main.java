3da0aa7c8dee63044adbaf4664215ee2245557fe
ASUS TUFF G <mohdhanzalak86@gmail.com>
2024-09-20 03:18:02 +0530

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+// Main Class (Spring Boot Entry Point)
+// Here’s the main Spring Boot application class:
+
+package com.example.xmlparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
